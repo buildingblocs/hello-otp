e7f6749a9f686ed48e239a8075a14ed66d337502
Aung Kaung Myat <69038584+icepcp@users.noreply.github.com>
2026-03-18 22:36:05 +0800

    update vite conf to fix build issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { cloudflare } from "@cloudflare/vite-plugin";
+// import { cloudflare } from "@cloudflare/vite-plugin";
 
-export default defineConfig({ plugins: [tailwindcss(), sveltekit(), cloudflare()] });
+export default defineConfig({ plugins: [tailwindcss(), sveltekit()] });
