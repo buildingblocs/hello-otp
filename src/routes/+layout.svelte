69784562b74ff3f17f28025b606fbe63db8ac5b3
Aung Kaung Myat <69038584+icepcp@users.noreply.github.com>
2026-03-18 23:18:25 +0800

    dark mode support + theme color

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,23 @@
 <script lang="ts">
-	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
-    import { Toaster } from "$lib/components/ui/sonner/index.js";
+  import "./layout.css";
+  import favicon from "$lib/assets/favicon.svg";
+  import { Toaster } from "$lib/components/ui/sonner/index.js";
 
-    let { data, children } = $props();
+  let { data, children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+  <link rel="icon" href={favicon} />
+  <meta
+    name="theme-color"
+    content="#ffffff"
+    media="(prefers-color-scheme: light)"
+  />
+  <meta
+    name="theme-color"
+    content="#000000"
+    media="(prefers-color-scheme: dark)"
+  />
+</svelte:head>
 <Toaster richColors />
 {@render children()}
